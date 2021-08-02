35ddf61d75afe3d56587a9aa3edeebac735e5332
Manfrin92 <manfredd_10@hotmail.com>
2021-08-02 20:02:11 -0300

    feat: asyncstorage functions

--- src/services/asyncStorage.ts
+++ src/services/asyncStorage.ts
@@ -2,6 +2,11 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Alert } from 'react-native';
 import { IGoal } from '../models/goal';
 
+export async function getStoredGoals(): Promise<IGoal[] | null> {
+    const storedGoals = await AsyncStorage.getItem('@goalTracker_goal');
+    return storedGoals ? JSON.parse(storedGoals) : null;
+}
+
 export async function storeGoals(goals: IGoal[]) {
     try {
         await AsyncStorage.setItem('@goalTracker_goal', JSON.stringify(goals));
@@ -12,17 +17,45 @@ export async function storeGoals(goals: IGoal[]) {
 
 export async function getStoredGoal(id: number) {
     try {
-        const storedGoalsRaw = await AsyncStorage.getItem('@goalTracker_goal');
+        const storedGoalsRaw = await getStoredGoals();
         if (storedGoalsRaw) {
-            const storedGoals: IGoal[] = JSON.parse(storedGoalsRaw);
-            if (storedGoals) {
-                const foundGoal = storedGoals.find(
-                    (storedGoal) => storedGoal.id === id
-                );
-                return foundGoal;
-            } else {
-                Alert.alert('Error', 'Goal not found');
-            }
+            const foundGoal = storedGoalsRaw.find(
+                (storedGoal) => storedGoal.id === id
+            );
+            return foundGoal;
+        } else {
+            Alert.alert('Error', 'Goal not found');
+        }
+    } catch (e) {
+        Alert.alert('Error while saving goal');
+    }
+}
+
+export async function deleteStoredGoal(id: number) {
+    try {
+        const storedGoalsRaw = await getStoredGoals();
+        if (storedGoalsRaw) {
+            const filteredGoals = storedGoalsRaw.filter(
+                (storedGoal) => storedGoal.id !== id
+            );
+            await storeGoals(filteredGoals);
+            return filteredGoals;
+        } else {
+            Alert.alert('Error', 'Goal not found');
+        }
+    } catch (e) {
+        Alert.alert('Error while saving goal');
+    }
+}
+
+export async function storeGoal(goal: IGoal) {
+    try {
+        const storedGoalsRaw = await getStoredGoals();
+        if (storedGoalsRaw) {
+            storedGoalsRaw.push(goal);
+            await storeGoals(storedGoalsRaw);
+        } else {
+            await storeGoals([goal]);
         }
     } catch (e) {
         Alert.alert('Error while saving goal');
